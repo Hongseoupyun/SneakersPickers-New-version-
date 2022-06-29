a77f3722d22c191d7e22aea11f3664956f7ed9f8
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-29 16:51:13 -0400

    commit

--- client/src/context/ApplicationProvider.js
+++ client/src/context/ApplicationProvider.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import ApplicationContext from "./ApplicationContext";
 
-
 const ApplicationProvider = ({children}) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const applicationProviderValues = {isLoggedIn, setIsLoggedIn}; 
+  
   return (
   <ApplicationContext.Provider value={applicationProviderValues}>
     {children}
