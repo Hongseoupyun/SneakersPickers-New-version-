df7a6e5ca74b3b8cda87f550ac2d7d208a27a969
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-30 13:45:10 -0400

    used prettier

--- client/src/components/MyAccount/MyListingItem.js
+++ client/src/components/MyAccount/MyListingItem.js
@@ -2,16 +2,12 @@ import React from "react";
 import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import { MdDescription } from "react-icons/md";
 
-
-
 export default function MyListingItem(props) {
-  const { name, description, brand, size, img, preference } = props
+  const { name, description, brand, size, img, preference } = props;
 
-
-  console.log("total offers:", props.total_offers)
+  console.log("total offers:", props.total_offers);
   return (
     <section className="mylisting-container">
-
       <img className="mylisting-img" src={img} />
 
       <div className="mylisting-card-contents">
@@ -23,21 +19,26 @@ export default function MyListingItem(props) {
           <div className="my-size">Size {size}</div>
         </div>
         <div className="listing-text" id="pref">
-          <BsFillBookmarkHeartFill /><span>Preference: {preference}</span>
+          <BsFillBookmarkHeartFill />
+          <span>Preference: {preference}</span>
         </div>
         <div className="listing-text" id="description">
-          <MdDescription /><span>{description}</span>
+          <MdDescription />
+          <span>{description}</span>
         </div>
         <div className="view-offers">
-          <a href="/offers"> <button type="submit">View Offers</button></a>
+          <a href="/offers">
+            {" "}
+            <button type="submit">View Offers</button>
+          </a>
         </div>
         <div className="listing-text" id="offers">
           <button type="button" className="btn btn-primary">
-            <span className="badge badge-light">{props.total_offers}</span> OFFERS
+            <span className="badge badge-light">{props.total_offers}</span>{" "}
+            OFFERS
           </button>
         </div>
       </div>
     </section>
-  )
-
+  );
 }
