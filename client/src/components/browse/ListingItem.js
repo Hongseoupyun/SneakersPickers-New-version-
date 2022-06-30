25932c87bec21214c49a24e848ab87fb70181bfa
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-30 16:26:30 -0400

    replaced <a> tag with useNavigate

--- client/src/components/browse/ListingItem.js
+++ client/src/components/browse/ListingItem.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./ListingItem.scss";
 import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import { MdDescription } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 export default function ListingItem(props) {
-
-  const { name, size, brand, description, preference, id, picture } = props
-  const LoggedIn = localStorage.getItem('isLoggedIn');
+  const navigate = useNavigate();
+  const handleClick = () => {
+    LoggedIn === "true" ? navigate(`/placeoffer/${id}`) : navigate("/login");
+  };
+  const { name, size, brand, description, preference, id, picture } = props;
+  const LoggedIn = localStorage.getItem("isLoggedIn");
   return (
     <section className="browse-container">
       <img className="mylisting-img" src={picture} />
@@ -15,21 +19,23 @@ export default function ListingItem(props) {
           <h1>{name}</h1>
         </div>
         <div className="listing-text" id="browse-pref">
-          <BsFillBookmarkHeartFill className="" /><span>Preference: {preference}</span>
+          <BsFillBookmarkHeartFill className="" />
+          <span>Preference: {preference}</span>
         </div>
         <div className="listing-text" id="brand-size-br">
           <div className="my-brand">{brand}</div>
           <div className="my-size">Size {size}</div>
         </div>
         <div className="listing-text" id="browse-desc">
-          <MdDescription /><span>{description}</span>
+          <MdDescription />
+          <span>{description}</span>
         </div>
         <div className="make-offers">
-           <a className="make-offer-button-href" href={LoggedIn === "true" ? `/placeoffer/${id}`:"/login"}> <button type="submit">Make a Offer</button></a>
+          <button type="submit" onClick={handleClick}>
+            Make a Offer
+          </button>
         </div>
       </div>
     </section>
   );
 }
-
-
