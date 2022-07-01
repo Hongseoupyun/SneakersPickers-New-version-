763e1ae03aa9662cfaec7c42fa48258c54538b39
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-30 22:01:14 -0400

    removed unnecessary codes

--- client/src/components/MyAccount/Offers.js
+++ client/src/components/MyAccount/Offers.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect} from "react";
 import "./Offers.scss"
 import axios from "axios";
 import MyOffers from "./MyOffers"
-import {ToastContainer, toast, Zoom, Bounce} from "react-toastify"
+import {ToastContainer, toast} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
 
