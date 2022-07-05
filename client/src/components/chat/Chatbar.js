c9ad6fb00a5680cd6b53ebbba9bdd74c694a8471
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-04 23:16:42 -0400

    added components for chating functions

---
+++ client/src/components/chat/Chatbar.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Chatbar() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+
+      </div>
+    </div>
+  )
+}
