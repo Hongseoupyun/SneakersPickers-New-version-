4ca4f9f9ed0b9a909e02f4291ad5cfe086ef1626
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-06 11:30:38 -0400

    added chat-online-badge div

--- client/src/components/chat/Conversation.js
+++ client/src/components/chat/Conversation.js
@@ -8,6 +8,7 @@ export default function Conversation() {
   return (
     <div className="conversation">
       <img className="conversation-img" src={testimg} alt="conversation-img" />
+      <div className="conversation-online-badge"></div>
       <span className="conversation-name">Air Jordan 1</span>
     </div>
   )
