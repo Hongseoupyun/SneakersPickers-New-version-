978c02b19cd9dfba913d92c173d510a966112794
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-07 12:29:29 -0400

    rendered offers to conversation box

--- client/src/components/chat/Conversation.js
+++ client/src/components/chat/Conversation.js
@@ -1,26 +1,21 @@
 import React from "react";
 import "./Conversation.scss";
 import testimg from "../images/aboutus-img3.jpeg";
-import axios from "axios"
 
-export default function Conversation() {
 
-  const loadOffer = function () {
-    axios.get("/api/offerlist/conversation")
-    .then((result)=>{
-      console.log(result)
-    })
-  }
+export default function Conversation(props) {
+
+  const { img, name } = props
 
   // const handleClick () {
 
   // }
 
   return (
-    <div className="conversation" onClick={loadOffer}>
-      <img className="conversation-img" src={testimg} alt="conversation-img" />
+    <div className="conversation" >
+      <img className="conversation-img" src={img} alt="conversation-img" />
       <div className="conversation-online-badge"></div>
-      <span className="conversation-name">Air Jordan 1</span>
+      <span className="conversation-name">{name}</span>
     </div>
   );
 }
