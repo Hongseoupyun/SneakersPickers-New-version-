aa4540365ea7cbc05a323a598a53934fb2036aa9
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-26 13:52:21 -0400

    added seeds

--- server/db/seeds/users.sql
+++ server/db/seeds/users.sql
@@ -13,11 +13,11 @@ INSERT INTO listings (user_id, name, description, brand, size, image_url, prefer
 VALUES
 
 
-  (2,'Sushi Shoe','Wear it or eat it, Gently used', 'New Balances',9, 'https://cdn4.dogonews.com/images/cc492c12-6072-4ed0-a94c-1979b8a654a2/tl-horizontal_main.jpg' , 'Jordans and Dunks', TRUE),
+  (2,'Sushi Shoe','Wear it or eat it, Gently used', 'Air Jordan',9, 'https://cdn4.dogonews.com/images/cc492c12-6072-4ed0-a94c-1979b8a654a2/tl-horizontal_main.jpg' , 'Jordans and Dunks', TRUE),
 
   (3,'nike dunk green','Never Worn..!', 'Nike Dunks',6, 'https://i.pinimg.com/564x/65/87/29/65872907e4b931db2cb6eeee4b9d8f4a.jpg' , 'Any Vans', TRUE),
 
-  (4,'Air force 1','Good Conditions', 'Air Jordan',6, 'https://i.pinimg.com/564x/92/3e/48/923e486bc2f913e8d8d06758a3a51dc8.jpg' , 'Jordans', TRUE);
+  (4,'Air force 1','Good Conditions', 'Nike Air Force',6, 'https://i.pinimg.com/564x/92/3e/48/923e486bc2f913e8d8d06758a3a51dc8.jpg' , 'Jordans', TRUE);
 
   
 
