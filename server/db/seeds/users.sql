1bed561e65f61a6cdac0dc38ffef77039cbaddb0
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-26 13:37:35 -0400

    added seeds

--- server/db/seeds/users.sql
+++ server/db/seeds/users.sql
@@ -4,7 +4,7 @@ VALUES
   ('test', 'test@test.com', 'test'),
   ('Nhan Tran','nhan@nhan.com','nhan'),
   ('Hongseoup Yun','hongseoup@hongseoup.com','hongseoup'),
-  ('Harsimran','harsimran@harsimran.com','harsimran'),
+  ('Junhwan Im','jun@jun.com','junhwan'),
   ('Harper Myers','erat.eget@yahoo.edu','Cnz7'),
   ('Yara Frank','sem@hotmail.edu','Hlz2');
 
@@ -15,7 +15,9 @@ VALUES
 
   (2,'Sushi Shoe','Wear it or eat it, Gently used', 'New Balance',9, 'https://cdn4.dogonews.com/images/cc492c12-6072-4ed0-a94c-1979b8a654a2/tl-horizontal_main.jpg' , 'Jordans and Dunks', TRUE),
 
-  (3,'nike dunk green','Never Worn..!', 'Nike Dunks',6, 'https://i.pinimg.com/564x/65/87/29/65872907e4b931db2cb6eeee4b9d8f4a.jpg' , 'Any Vans', TRUE);
+  (3,'nike dunk green','Never Worn..!', 'Nike Dunks',6, 'https://i.pinimg.com/564x/65/87/29/65872907e4b931db2cb6eeee4b9d8f4a.jpg' , 'Any Vans', TRUE),
+
+  (4,'nike dunk green','Good Conditions', 'Air Jordan',6, 'https://i.pinimg.com/564x/92/3e/48/923e486bc2f913e8d8d06758a3a51dc8.jpg' , 'Jordans', TRUE);
 
   
 
